606c10df3b774a14368cc901bb3999f17f6fb651
26watsonkl <26watsonkl@marshallisd.com>
2024-09-19 13:17:36 -0500

    +Class Code

---
+++ C#Console/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace C_console;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World.");
+        // =   Var.myVar();
+        // Var.DataType();
+        //Math.Numbers();
+        //Math.Add(100, 450, 123);
+        //Input.Hello();
+        //If.Numbers(10);
+        //If.Health(0);
+        // Loops.ForLoops();
+        // Loops.EvenLoop();
+        // Loops.HealthLoop();
+        // Loops.WhileLoop();
+        //int result2 = Return1.PrintSquare();
+        //Console.WriteLine();
+        //Return1.printSquare();
+        //Return1.CheckEvenOdd(5);
+        //int result = Return1.AddNumber(3,6);
+        //Console.WriteLine(result);
+        //Loops.NestedLoops();
+        //Array.Array1(arg);
+    }
+}
---
+++ C#Console/Var.cs
@@ -0,0 +1,33 @@
+
+class Var
+{
+    public static void myVar()
+    {
+        string myName = "Karson";
+        Console.WriteLine(myName);
+    }
+
+    public static void DataType()
+    {
+        string firstName = "Karson";
+        string lastName = "Watson";
+        string wholeName = firstName + lastName;
+        int age = 16;
+        float gpa = 4.4f;
+        double height = 6.2;
+        decimal dollar = 100.95m;
+        bool is_teacher = true;
+        char grade = 'f';
+
+        // Console.WriteLine(firstName);
+        // Console.WriteLine(lastName);
+        Console.WriteLine(wholeName);
+        Console.WriteLine(age);
+        Console.WriteLine(gpa);
+        Console.WriteLine(height);
+        Console.WriteLine(dollar);
+        Console.WriteLine(is_teacher);
+        Console.WriteLine(grade);
+    }
+    
+}
